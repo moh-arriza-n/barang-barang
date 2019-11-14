3e87cc0e0d17be4b80240098541ff3c72e7f0d97
Moh-Arriza <moh-arriza-n@users.noreply.github.com>
2019-11-14 12:08:42 +0700

    editing form listing program

--- form_2.php
+++ form_2.php
@@ -1,14 +1,18 @@
 <?php
 include('koneksi.php');
-if(isset($_POST['simpan'])){
-	$query_tambah_data="insert into barang(MERK,HARGA)values
-	('".$_POST['MERK']."',
-		'".$_POST['HARGA']."')";
+if(isset($_POST['input'])){
+	$query_tambah_data="insert into catatan_transaksi(TOKO,ALAMAT,NAMA_BARANG,QTY,HARGA,TOTAL_HARGA)values
+	('".$_POST['TOKO']."',
+		'".$_POST['ALAMAT']."',
+		'".$_POST['NAMA_BARANG']."',
+		'".$_POST['QTY']."',
+		'".$_POST['HARGA']."',
+		'".$_POST['TOTAL_HARGA']."')";
 		$proses_data=mysql_query($query_tambah_data);
 if ($proses_data){
-		echo 'tambah data berhasil (OK)';
+		echo 'penambahan data dapat dilakukan (OK)';
 }else{
-		echo mysql_error();
+		echo 'penambahan data tidak dapat dilakukan (ERROR)';
 }
 }
 ?>
@@ -16,19 +20,35 @@ if ($proses_data){
 <form method="POST" action="">
 <table>
 		<tr>
-				<td>ID Barang</td>
-				<td><input name="ID_BARANG" type="number"></td>
+				<td>NO_Transaksi</td>
+				<td><input name="NO_TRANSAKSI" type="number"></td>
 		</tr>
 		<tr>
-				<td>Merk</td>
-				<td><input name="MERK" type="text"></td>
+				<td>Toko</td>
+				<td><input name="TOKO" type="text"></td>
+		</tr>
+		<tr>
+				<td>Alamat</td>
+				<td><input name="ALAMAT" type="text"></td>
+		</tr>
+		<tr>
+				<td>Nama Barang</td>
+				<td><input name="NAMA_BARANG" type="text"></td>
+		</tr>
+		<tr>
+				<td>QTY</td>
+				<td><input name="QTY" type="number"></td>
 		</tr>
 		<tr>
 				<td>Harga</td>
 				<td><input name="HARGA" type="number"></td>
 		</tr>
 		<tr>
-			<td><input name="simpan" type="submit"></td>
+				<td>Total Harga</td>
+				<td><input name="TOTAL_HARGA" type="number"></td>
+		</tr>
+		<tr>
+			<td><input name="input" type="submit"></td>
 		</tr>
 	</table>
 </form>
